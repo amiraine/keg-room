20aa2c7933b9cc99035a030e29e56f8d44eb5039
ami cooper <amiraine.cooper@gmail.com>
2018-05-03 17:00:39 -0700

    forgot what i did, css editing bootstrap didn't work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,20 +28,19 @@ export class AppComponent {
 //beer display
   taps: Taps[] = [
     new Taps('Ecliptic Starburst IPA', 'Ecliptic', 6, 7.8,124),
-    new Taps('Level Let\'s Play Pilsner', 'Level Brewing', 6, 4.9,124),
+    new Taps('Level Let\'s Play Pilsner', 'Level Brewing', 6, 4.9,6),
     new Taps('Ninkasi Yours Truly Pale Ale','Ninkasi', 6, 4.3,124),
-    new Taps('Logsdon Seizoen','Logsdon',5,7.5,124),
+    new Taps('Logsdon Seizoen','Logsdon',5,7.5,98),
     new Taps('Pabst Blue Ribbon', 'Pabst Brewing',2,4.7,124),
-    new Taps('Passion Statement','2 Towns Ciderhouse',5,5.2,124),
-    new Taps('Wild Plum', 'WildCraft Cidery',5,7.5,124),
+    new Taps('Passion Statement','2 Towns Ciderhouse',5,5.2,40),
+    new Taps('Wild Plum', 'WildCraft Cidery',5,7.5,81),
   ];
-  till: Till[] = [
-    new Till(150,0,0)
-  ]
   //beer edits and adds
   refill(currentTap){
     if (currentTap.volume <=10){
       return "bg-danger";
+    } else if (currentTap.volume <=50 && currentTap.volume >10){
+      return "bg-warning";
     }
   }
   selectedTap = null;
